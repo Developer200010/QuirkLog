df0c887f241aff2769c7b6b2ebe257d88796b270
chandan vishwakarma <chandanvishw11002@gmail.com>
2025-08-22 00:41:19 +0530

    cors setup done for production level

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const cors = require("cors");
+const path = require("path");
 
 
 // Middlewares
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -15,6 +18,27 @@ const postRoute = require("./routes/postRoute.js")
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/post", postRoute);
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5000",
+      credentials: true,
+    })
+  );
+  // Serve frontend
+  app.use(express.static(path.join(__dirname, "./frontend/dist")));
+  app.get("/*any", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./", "frontend", "dist", "index.html"));
+  });
+} else {
+  app.use(
+    cors({
+      credentials: true,
+    })
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
